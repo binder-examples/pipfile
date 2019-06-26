9be798fc65cc13e3ffccf6d3f5081212381a94f0
Erik Sundell <erik.i.sundell@gmail.com>
2019-06-26 21:19:06 +0200

    Add demonstrative test of python ver

--- tests/test_installation_of_dependencies.py
+++ tests/test_installation_of_dependencies.py
@@ -2,3 +2,7 @@ def test_dependencies_are_installed():
     import numpy
     import seaborn
     print("Wieee!")
+
+def test_pipfile_specified_python_version():
+    import sys
+    assert sys.version_info[:2] == (3, 6)
